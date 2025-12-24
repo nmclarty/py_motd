4a6e8a04771353a8fedc42a2e9c06df3c38c7ad9
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2025-12-24 00:33:00 -0800

    Added settings

--- nix/modules/nixos/sops-podman.nix
+++ nix/modules/nixos/sops-podman.nix
@@ -6,14 +6,25 @@ in
 {
   options.services.sops-podman = {
     enable = mkEnableOption "Enable loading sops secrets to podman.";
+    settings = {
+      sopsFile = mkOption {
+        types = types.str;
+        description = "The path the the encrypted sops file to be loaded from."
+      };
+    };
   };
   config = mkIf cfg.enable {
+    sops.secrets."sops-podman.yaml" = {
+      restartUnits = [ "sops-podman.service" ];
+      sopsFile = cfg.settings.sopsFile;
+      key = "";
+    };
     systemd.services.sops-podman = {
       description = "Load system managed secrets into the podman secret store";
       before = [ "network-online.target" ];
       wantedBy = [ "network-online.target" ];
       serviceConfig = {
-        ExecStart = "${perSystem.nix-helpers.default}/bin/sops_podman";
+        ExecStart = "${perSystem.nix-helpers.default}/bin/sops_podman ${cfg.sops.secrets."sops-podman.yaml".path}";
       };
     };
   };
